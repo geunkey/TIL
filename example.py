69ee2f7f000b586569150b81e4deb36405ed82e2
김근호 <geunkey2243@gmail.com>
2022-01-26 03:12:15 +0900

    example

--- example.py
+++ example.py
@@ -1,5 +1,7 @@
-count_vowels = 'apple'
-print(count_vowels.count('p'))
+num = int(input('숫자를 입력해주세요 : '))
+print(num, type(num))
 
-count_vowels = 'banana'
-print(count_vowels.count('a'))
+if num % 2:
+    print('홀수입니다.')
+else:
+    print('짝수입니다.')
