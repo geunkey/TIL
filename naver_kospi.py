6432937825df445b9415bd4641dd9426d1450b65
김근호 <geunkey2243@gmail.com>
2022-01-27 18:52:12 +0900

    update220127

---
+++ naver_kospi.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://finance.naver.com/sise/'
+response = requests.get(url).text
+data = BeautifulSoup(response, 'html.parser')
+#print(response)
+#print(data)
+kospi = data.select_one('#KOSPI_now')
+print(f'현재의 코스피 지수를 {kospi.text}입니다.')
