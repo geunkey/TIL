912867ac9db298b97eb446895db43a3f6ffd7bee
김근호 <geunkey2243@gmail.com>
2022-02-06 18:50:11 +0900

    220206

--- naver_kospi.py
+++ naver_kospi.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 url = 'https://finance.naver.com/sise/'
 response = requests.get(url).text
-data = BeautifulSoup(response, 'html.parser')
+data = BeautifulSoup(response,'html.parser')
 #print(response)
 #print(data)
 kospi = data.select_one('#KOSPI_now')
