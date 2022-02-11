c13a0376c0b9b4cb21c5af91d9e3b24a942fb77f
김근호 <geunkey2243@gmail.com>
2022-02-11 18:12:11 +0900

    220211

---
+++ 7step10809-1.py
@@ -0,0 +1,6 @@
+S = input()
+
+# lst = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+for i in alpha:
+    print(S.find(i), end=' ')
