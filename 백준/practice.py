73cbda3c299ca9fd3fe7c6f601baa8fee131878e
김근호 <geunkey2243@gmail.com>
2022-05-03 01:29:20 +0900

    220503

--- 백준/practice.py
+++ 백준/practice.py
@@ -11,18 +11,3 @@ while True:
         break
 
 print(cnt)
-#
-# def f(N):
-#     global cnt, sol
-#     a = N // 10
-#     b = N % 10
-#     sol = a + b
-#     N = (b * 10) + (sol % 10)
-#     cnt += 1
-#
-#
-# N = int(input())
-# f(N)
-# result = 0
-# cnt = 0
-# print(cnt)
